c1757d69e590b0b70dfa4d9d22e46d0a80bde45a
kiboem <63495505+Song-ki-beom@users.noreply.github.com>
2024-07-26 22:12:14 +0900

    UMG 마무리

--- Source/PuzzlePlatforms/PuzzlePlatformGameInstance.cpp
+++ Source/PuzzlePlatforms/PuzzlePlatformGameInstance.cpp
@@ -2,16 +2,54 @@
 
 
 #include "PuzzlePlatformGameInstance.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+#include "PlatformTrigger.h"
 #include "Engine/Engine.h"
+#include "Widgets/SWidget.h"
+#include "GameFramework/PlayerController.h"
 UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer) 
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance Constructor"));
+
+	static ConstructorHelpers::FClassFinder<UUserWidget> MenuBPClass(TEXT("/Game/MenuSystem/WBP_MainMenu"));
+	if (!ensure(MenuBPClass.Class != nullptr)) return;
+
+	MenuClass = MenuBPClass.Class;
+
+	//UE_LOG(LogTemp, Warning, TEXT("Found class %s"),*PlatformTriggerBPClass.Class->GetName()); //클래스 객체에 대한 포인터 
+
+
+
 }
 
 void UPuzzlePlatformGameInstance::Init()
 {
-	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+	UE_LOG(LogTemp, Warning, TEXT("Found class %s"), *MenuClass->GetName());
 
+
+	
+
+}
+
+void UPuzzlePlatformGameInstance::LoadMenu()
+{
+	if (!ensure(MenuClass != nullptr)) return;
+	UUserWidget* Menu = CreateWidget<UUserWidget>(this, MenuClass);
+	if (!ensure(Menu != nullptr)) return;
+
+	
+
+	Menu->AddToViewport();
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	FInputModeUIOnly InputModeData;
+	InputModeData.SetWidgetToFocus(Menu->TakeWidget());
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+
+	PlayerController->SetInputMode(InputModeData);
+	PlayerController->bShowMouseCursor = true;
 
 }
 
@@ -21,6 +59,13 @@ void UPuzzlePlatformGameInstance::Host()
 	if (!ensure(Engine != nullptr)) return;
 	Engine->AddOnScreenDebugMessage(0, 5.f, FColor::Green, TEXT("This is host"));
 
+	UWorld* World = GetWorld();
+	if (!ensure(World != nullptr)) return;
+
+	World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen"); 
+
+	//listen <<- 플레이어가 직접 호스팅하는 서버 
+
 }
 
 void UPuzzlePlatformGameInstance::Join(const FString& Address)
@@ -29,5 +74,12 @@ void UPuzzlePlatformGameInstance::Join(const FString& Address)
 	if (!ensure(Engine != nullptr)) return;
 	Engine->AddOnScreenDebugMessage(0, 5.f, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
 
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, TRAVEL_Absolute);
+
 
 }
+
+
--- Source/PuzzlePlatforms/PuzzlePlatformGameInstance.h
+++ Source/PuzzlePlatforms/PuzzlePlatformGameInstance.h
@@ -9,6 +9,9 @@
 /**
  * 
  */
+
+//FclassFinder :FClassFinder는 컴파일 타임에 에셋을 찾고 로드할 때 사용하는 템플릿 클래스 
+
 UCLASS()
 class PUZZLEPLATFORMS_API UPuzzlePlatformGameInstance : public UGameInstance
 {
@@ -20,6 +23,13 @@ public:		UPuzzlePlatformGameInstance
 		  const FObjectInitializer& ObjectInitializer
 	  );
 	virtual void Init();
+
+
+	UFUNCTION(BlueprintCallable)
+		void LoadMenu();
+
+
+
 		
 	UFUNCTION(Exec) //Exec : 게임 내 콘솔이나 개발자 명령을 통해 직접 호출할 수 있는 함수를 정의 , GameInstance에서 호출 가능 
 		void Host();
@@ -27,4 +37,7 @@ public:		UPuzzlePlatformGameInstance
 	UFUNCTION(Exec)
 		void Join(const FString& Address);
 
+private:
+	TSubclassOf<class UUserWidget> MenuClass;
+	
 };
